3c6b890c7c7c90cb1f357f083a5c3e589e6766ef
kujawapatryk <homik363@wp.pl>
2023-02-04 16:53:23 +0100

    Added error handling

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import express, {json} from "express";
+import express, {json, Request, Response} from "express";
 import cors from "cors";
 import "express-async-errors";
+import {handleError} from "./utilits/errors";
 
 
 const app =express();
@@ -9,6 +10,12 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(json());
+
+app.get('/', async (req:Request, res:Response)=>{
+    throw new Error('Daaamn!');
+    }
+)
+app.use(handleError);
 
 app.listen(3001,'0.0.0.0', () =>{
     console.log('Listening on http://localhost:3001');
