64c16fb4eb4e3867af851434b49bccdf4a2c58d8
kujawapatryk <homik363@wp.pl>
2023-02-09 01:01:28 +0100

    Added rate limiter

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, {json, Request, Response} from "express";
 import cors from "cors";
 import "express-async-errors";
 import {handleError} from "./utilits/errors";
+import rateLimit from "express-rate-limit";
 
 
 const app =express();
@@ -10,11 +11,15 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(json());
+app.use(rateLimit({
+    windowMs: 5*60 *1000,
+    max: 100,
+}))
 
-app.get('/', async (req:Request, res:Response)=>{
-    throw new Error('Daaamn!');
-    }
-)
+// app.get('/', async (req:Request, res:Response)=>{
+//     throw new Error('Daaamn!');
+//     }
+// )
 app.use(handleError);
 
 app.listen(3001,'0.0.0.0', () =>{
